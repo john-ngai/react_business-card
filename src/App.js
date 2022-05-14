89839d43665ea009daab30a7db5698178cbc179f
mrjohnming <john.ngai@hotmail.com>
2022-05-14 18:57:41 -0400

    Import components - About & Interests

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Info from './components/Info';
+import About from './components/About';
+import Interests from './components/Interests';
 
 export default function App() {
   return (
     <div className="container">
       <Info />
+      <About />
+      <Interests />
     </div>
   );
 }
