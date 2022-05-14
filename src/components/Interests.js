f9ef840c1f4470140460b4b16504e30720e5162c
mrjohnming <john.ngai@hotmail.com>
2022-05-14 18:56:13 -0400

    Create components - About & Interests

---
+++ src/components/Interests.js
@@ -0,0 +1,11 @@
+export default function Interests() {
+  return (
+    <div className="container--interests">
+      <h1>Interests</h1>
+      <p>
+        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. 
+        Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+      </p>
+    </div>
+  );
+}
