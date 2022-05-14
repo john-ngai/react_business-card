b0c728f35a2f8b27e185aec87dc61b8fb6e0f411
mrjohnming <john.ngai@hotmail.com>
2022-05-14 19:57:44 -0400

    Add hyperlinks

--- src/components/Info.js
+++ src/components/Info.js
@@ -6,20 +6,34 @@ export default function Info() {
   return (
     <div className="container--info">
       <img className='portrait' src={Portrait} alt='portrait' />
-      
+
       <h1 className='name'>John Ngai</h1>
       <h2 className='title'>Full Stack Developer</h2>
-      <p className='website'>JohnNgai.com</p>
+
+      <a href='http://johnngai.com/'
+        target='_blank' rel='noreferrer'
+      >
+        <p className='website'>JohnNgai.com</p>
+      </a>
 
       <div className='buttons'>
-        <span className='button--email'>
-          <img className='icon--mail' src={MailIcon} alt='mail icon' />
-          <p>Email</p>
-        </span>
-        <span className='button--linkedin'>
-          <img className='icon--linkedin' src={LinkedInIcon} alt='LinkedIn icon' />
-          <p>LinkedIn</p>
-        </span>
+        <a href='mailto:john.ngai@hotmail.com'
+          target='_blank' rel='noreferrer'
+        >
+          <span className='button--email'>
+            <img className='icon--mail' src={MailIcon} alt='mail icon' />
+            <p>Email</p>
+          </span>
+        </a>
+
+        <a href='https://www.linkedin.com/in/john-ngai/'
+          target='_blank' rel='noreferrer'
+        >
+          <span className='button--linkedin'>
+            <img className='icon--linkedin' src={LinkedInIcon} alt='LinkedIn icon' />
+            <p>LinkedIn</p>
+          </span>
+        </a>
       </div>
     </div>
   );
